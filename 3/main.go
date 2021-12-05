5bde12be3f6d8c577d55ae4c1a4027a8eeea1075
Armando Garcia-Jacquier <armando.garcia-jacquier@nytimes.com>
2021-12-04 22:22:39 -0500

    First part clumsy again

--- 3/main.go
+++ 3/main.go
@@ -32,6 +32,14 @@ func main()  {
 	epsylonNum, err := strconv.ParseInt(strings.Trim(strings.Replace(fmt.Sprint(epsylon), " ", "", -1), "[]"), 2, 32)
 
 	fmt.Printf("Result\nZeros: %v\nOnes: %v\nGama: %v\nGamaNum: %v\nEpsylon: %v\nRate: %v\n", zerosCount, onesCount, gama, gamaNum, epsylon, gamaNum * epsylonNum)
+	o2Num, err := strconv.ParseInt(reduceListByCriteria("o2", inputList), 2, 64)
+	co2Num, err := strconv.ParseInt(reduceListByCriteria("co2", inputList), 2, 64)
+	fmt.Println("o2", o2Num)
+	fmt.Println("co2", co2Num)
+
+	o2, _ := strconv.ParseInt(filterListByCriteria("o2", inputList), 2, 64)
+	co2, _ := strconv.ParseInt(filterListByCriteria("co2", inputList), 2, 64)
+	fmt.Printf("Result Using Filter\nO2:%d\nCo2:%d\nProduct:%d\n", o2, co2, o2 * co2)
 }
 
 // take the input and turn it into a list
@@ -80,4 +88,99 @@ func greater(num1 int, num2 int) bool {
 
 func lesser(num1 int, num2 int) bool {
 	return num1 < num2
+}
+
+// use tally allNums
+// o2 get the most common, if equal favour 1
+// co2 get the least common, if equal favour 0
+
+// countListByPosition - traverses a list and returns a list of elements that match the comparison
+func reduceListByCriteria(criteria string, list []string) string {
+	// get count of 0s and 1s
+	zerosCount, onesCount := tallyAllNumbers(list)
+	compare := greater
+	if criteria != "o2" {
+		compare = lesser
+	}
+	prioritize := "1"
+	//delegate := "0"
+	if criteria != "o2" {
+		prioritize = "0"
+		//delegate = "1"
+	}
+
+	// create the values that are expected
+	expected := ""
+	for i, _ := range zerosCount {
+		if zerosCount[i] == onesCount[i] {
+			expected += prioritize
+		} else if compare(zerosCount[i], onesCount[i]) {
+			expected += "0"
+		} else {
+			expected += "1"
+		}
+	}
+	fmt.Println("This is expected", expected)
+
+	filtered := make(map[string]string)
+	for _, num := range list {
+		filtered[num] = num
+	}
+
+	for key, char := range expected {
+		for currNum := range filtered {
+			if string(currNum[key]) != string(char) {
+				delete(filtered, currNum)
+			}
+			if len(filtered) == 1 {
+				fmt.Println(filtered, len(filtered))
+				for _, res := range filtered {
+					return res
+				}
+			}
+		}
+	}
+
+	return ""
+}
+
+// countListByPosition - traverses a list and returns a list of elements that match the comparison
+func filterListByCriteria(criteria string, list []string) string {
+	// get count of 0s and 1s
+	compare := greater
+	if criteria != "o2" {
+		compare = lesser
+	}
+
+	// go through the list and separate the numbers based on 1 and 0 for given digit
+	filteredList := list
+	for i := 0; i < len(list[0]); i++ {
+		zeroList := []string{}
+		oneList := []string{}
+		for _, num := range filteredList {
+			if string(num[i]) == "0" {
+				zeroList = append(zeroList, num)
+			} else {
+				oneList = append(oneList, num)
+			}
+		}
+		if len(zeroList) == 0 {
+			filteredList = oneList
+		} else if len(oneList) == 0 {
+			filteredList = zeroList
+		} else if len(zeroList) == len(oneList) {
+			if criteria == "o2" {
+				filteredList = oneList
+			} else {
+				filteredList = zeroList
+			}
+		} else if compare(len(zeroList), len(oneList)) {
+			filteredList = zeroList
+		} else {
+			filteredList = oneList
+		}
+	}
+
+	//fmt.Println("list", list, "  filetered", filteredList)
+	return filteredList[0]
 }
---
+++ 4/main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main()  {
+	if len(os.Args) < 2 {
+		fmt.Println("Provide input file")
+		return
+	}
+	inputFilename := os.Args[1]
+	input, err := os.ReadFile(inputFilename)
+	if err != nil {
+		fmt.Println("Error reading file: ", err)
+		return
+	}
+
+	list := strings.Split(string(input),"\n\n")
+	drawnNums := strings.Split(list[0], ",")
+	boards := buildBoards(list[1:])
+	//fmt.Printf("Nums: %s\nBoards: %v\n", drawnNums, boards)
+
+	var boardStructs []BoardMap
+	for _, board := range boards {
+		boardStructs = append(boardStructs, buildBoardMap(board))
+	}
+	fmt.Printf("%+v\n  %d", boardStructs[0], len(boardStructs[0].Nums))
+	for _, drawnNum := range drawnNums {
+		for _, bStruct := range boardStructs {
+			checkAndAdd(bStruct, drawnNum)
+			bingo := checkBingo(bStruct, drawnNum)
+			delete(bStruct.Nums, drawnNum)
+			if bingo {
+				rems := calcAllRemaining(bStruct)
+				currNum, err := strconv.Atoi(drawnNum)
+				if err != nil {
+					panic("Failed to convert drawn num")
+				}
+				fmt.Printf("Result\nRems: %v\nCurrNum: %v\nProd: %v\nBoard: %+v", rems, drawnNums, rems * currNum, bStruct)
+				return
+			}
+		}
+	}
+}
+
+func buildBoards(list []string) [][][]string {
+	var res [][][]string
+
+	for _, board := range list {
+		temp := strings.Split(board, "\n")
+		var currBoard [][]string
+		for _, row := range temp {
+			currBoard = append(currBoard, strings.Fields(row))
+		}
+		res = append(res, currBoard)
+	}
+	return res
+}
+
+
+
+// take the input and separate it into the drawn nums and boards
+// for each board we create a map of the numbers and their position
+// each row and column get a counter once that counter meets 5 it's a bingo
+
+// create a map of each board
+	// contains maps for the nums-> position, col count, row count
+// when a number is present we get coords and update the row and col counters if it is complete we call bingo
+type BoardMap struct {
+	Nums map[string][]int // num: [x, y] coords
+	Rows map[int]int // row position and count of found numbers
+	Cols map[int]int
+}
+
+func buildBoardMap(board [][]string) BoardMap {
+	nums := map[string][]int{}
+	rows := map[int]int{}
+	cols := map[int]int{}
+
+	for rowPos, rowList := range board {
+		rows[rowPos] = 0
+		cols[rowPos] = 0 // being a square, using the row position to initialize the column map as well
+
+		for col, colVal := range rowList {
+			nums[colVal] = []int{rowPos, col}
+		}
+	}
+
+	return BoardMap{
+		Nums: nums,
+		Rows: rows,
+		Cols: cols,
+	}
+}
+
+func checkAndAdd(board BoardMap, num string) {
+	if board.Nums[num] != nil {
+		coords := board.Nums[num]
+		board.Rows[coords[0]] += 1
+		board.Cols[coords[1]] += 1
+	}
+}
+
+func checkBingo(board BoardMap, num string) bool {
+	if board.Nums[num] != nil {
+		coords := board.Nums[num]
+		if board.Rows[coords[0]] > 4 || board.Cols[coords[1]] > 4 {
+			return true
+		}
+	}
+	return false
+}
+
+func calcAllRemaining(board BoardMap) int {
+	tot := 0
+	for num, _ := range board.Nums {
+		curr, err := strconv.Atoi(num)
+		if err != nil {
+			panic(err)
+		}
+		tot += curr
+	}
+	return tot
+}
